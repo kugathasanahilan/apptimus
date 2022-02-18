1087b93529cde255d486ee975c7c0c908411ff01
ahilan98 <kugathasanahilan@gmail.com>
2022-02-18 10:25:28 +0530

    first commit

---
+++ Ahilan/app/Http/Controllers/CustomerController.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Customer;
+
+class CustomerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        $customer = Customer::all();
+        return view('customer.index', compact('customer'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+        return view('customer/create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+        $data = $request -> all;
+        $fna=$request->input('fname');
+        $lna=$request->input('lname');
+        $address=$request->input('address');
+
+        $customer=new Customer();
+        $customer->first_name=$fna;
+        $customer->last_name=$lna;
+        $customer->address=$address;
+       
+        $customer->save();
+         return redirect('customers/create')->with('status','User Created!');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+        $customer=Customer::find($id);
+       return view('customer.show',compact('customer'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ Ahilan/resources/views/customer/show.blade.php
@@ -0,0 +1,6 @@
+<link rel="stylesheet" href="{{asset('css/app.css')}}">
+<h1 style="text-align:center">{{$customer->first_name}} Details</h1>
+<p><b>ID: </b>{{$customer->id}}</p>
+<p><b>First Name: </b>{{$customer->first_name}}</p>
+<p><b>Last Name: </b>{{$customer->last_name}}</p>
+<p><b>Customer: </b>{{$customer->address}}</p>
