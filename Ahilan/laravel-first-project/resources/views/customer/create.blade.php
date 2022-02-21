29236824920d05fb787ccc8e0eb13acc49a5fea1
ahilan98 <kugathasanahilan@gmail.com>
2022-02-21 12:27:06 +0530

    error bootstrap

--- Ahilan/laravel-first-project/resources/views/customer/create.blade.php
+++ Ahilan/laravel-first-project/resources/views/customer/create.blade.php
@@ -9,36 +9,31 @@
     <div class="alert alert-danger">
         {{ session('messages') }}
     </div>
-@endif
-<!-- @if($errors->any())
-@foreach($errors->all() as $err)
-<li style="color:red;">{{$err}}</li>
-@endforeach
-@endif -->
+@endif 
 <h1>Create User</h1>
 <form action="{{route('customers.store')}}" method="post" >
     @csrf
-<div class="input-group mb-3">
-  <span class="input-group-text" id="basic-addon1">First Name</span>
-  <input type="text" class="form-control" placeholder="First Name" name="first_name" aria-label="firstname" aria-describedby="basic-addon1">
+    <label for="validationServer03" class="form-label">First Name</label>
+    <input type="text" class="form-control @if($errors->has('first_name'))is-invalid @endif" name="first_name" id="validationServer03" value="{{old('first_name')}}" aria-describedby="validationServer03Feedback" required>
+    <div id="validationServer03Feedback" class="invalid-feedback">
+    @error('first_name')
+    {{$message}}
+    @enderror
+    </div>
+<label for="validationServer03" class="form-label">Last Name</label>
+    <input type="text" class="form-control  @if($errors->has('last_name'))is-invalid @endif" name="last_name" id="validationServer03" aria-describedby="validationServer03Feedback" value="{{old('last_name')}}" required>
+    <div id="validationServer03Feedback" class="invalid-feedback">
+@error('last_name')
+{{$message}}
+@enderror
 </div>
-<p style="color:red">
-{{$errors->has('first_name') ? $errors->first('first_name') : ''}}   
-</p>
-<div class="input-group mb-3">
-  <span class="input-group-text" id="basic-addon1">Last Name</span>
-  <input type="text" class="form-control" placeholder="Last Name" name="last_name" aria-label="lastname" aria-describedby="basic-addon1">
+<label for="validationServer03" class="form-label">Address</label>
+    <input type="text" class="form-control  @if($errors->has('address'))is-invalid @endif" name="address" id="validationServer03" aria-describedby="validationServer03Feedback" value="{{old('address')}}" required>
+    <div id="validationServer03Feedback" class="invalid-feedback">
+@error('address')
+{{$message}}
+@enderror
 </div>
-<p style="color:red">
-{{$errors->has('last_name') ? $errors->first('last_name') : ''}}
-</p>
-<div class="input-group mb-3">
-  <span class="input-group-text" id="basic-addon1">Address</span>
-  <textarea class="form-control" placeholder="Address" aria-label="address" name="address" aria-describedby="basic-addon1"></textarea>
-</div>
-<p style="color:red">
-{{$errors->has('address') ? $errors->first('address') : ''}}
-</p>
+<br>
 <input type="submit" value="submit" class="btn btn-success float-end">
 </form>
-  </div>
